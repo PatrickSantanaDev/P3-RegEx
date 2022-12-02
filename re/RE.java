bf1f1168e59d919c57770dbbbc62845fdb15e378
Patrick Santana <85718492+PatrickSantanaDev@users.noreply.github.com>
2022-12-01 21:50:10 -0700

    update

---
+++ re/RE.java
@@ -0,0 +1,17 @@
+package re;
+
+import fa.nfa.NFA;
+
+public class RE implements REInterface {
+	
+	public RE(String regEx) {
+		
+	}
+	
+	@Override
+	public NFA getNFA() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
